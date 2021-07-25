343ff174fb4b1df8937fb2b46e6edf9ff79699e2
teerawat prarom <teewat@yahoo.com>
2021-07-26 00:36:05 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const writeCards = (names,eventName)=>{
+    const cardMsg = [];
+    for(let i = 0; i < names.length; i++){
+        cardMsg[i] = 'Thank you, '+names[i]+', for the wonderful '+eventName+' gift!'; 
+    }
+    return cardMsg
+}
+
+const countDown = (n)=>{
+    while(n >= 0){
+        console.log(n);
+        n--;
+    }
+}
